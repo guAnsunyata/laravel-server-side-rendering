c6380bb2b3b2912843eb04763999156966064923
guansunyata <guansunyata@gmail.com>
2018-11-01 18:31:04 +0800

    update: default ssr enabled config

--- config/ssr.php
+++ config/ssr.php
@@ -4,7 +4,7 @@ return [
     /*
      * Enable or disable the server renderer. Enabled in production by default.
      */
-    'enabled' => env('APP_ENV') === 'production',
+    'enabled' => env('ENABLE_SSR', true),
 
     /*
      * When server side rendering goes wrong, nothing will be rendered so the
